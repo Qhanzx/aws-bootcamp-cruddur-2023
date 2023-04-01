6369627c62d69dfabc1ad29e0831d477c0f15ba9
Qhanzx <125101628+Qhanzx@users.noreply.github.com>
2023-04-01 01:25:14 +0100

    update home_activities

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -4,7 +4,41 @@ from opentelemetry import trace
 tracer = trace.get_tracer("home.activities")
 
 class HomeActivities:
-  #def run(logger):
+  def run(cognito_user_id=None):
+    print("HOME ACTIVITY")
+    #logger.info("HomeActivities")
+    with tracer.start_as_current_span("home-activites-mock-data"):
+      span = trace.get_current_span()
+      now = datetime.now(timezone.utc).astimezone()
+      span.set_attribute("app.now", now.isoformat())
+
+      sql = query_wrap_array("""
+      SELECT
+        activities.uuid,
+        users.display_name,
+        users.handle,
+        activities.message,
+        activities.replies_count,
+        activities.reposts_count,
+        activities.likes_count,
+        activities.reply_to_activity_uuid,
+        activities.expires_at,
+        activities.created_at
+      FROM public.activities
+      LEFT JOIN public.users ON users.uuid = activities.user_uuid
+      ORDER BY activities.created_at DESC
+      """)
+      print(sql)
+      with pool.connection() as conn:
+        with conn.cursor() as cur:
+          cur.execute(sql)
+          # this will return a tuple
+          # the first field being the data
+          json = cur.fetchone()
+      return json[0]
+    
+    
+  '''#def run(logger):
   def run(cognito_user_id=None):
     #logger.info('HomeActivities')
     with tracer.start_as_current_span("home_activities_data"):
@@ -52,5 +86,5 @@ class HomeActivities:
       }
       ]
       span.set_attribute("user.length", len(results))   
-      return results
+      return results'''
          
